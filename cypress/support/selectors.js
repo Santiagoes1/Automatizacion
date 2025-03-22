60455e9c6a72e24290a58274bcabdb15a05707c2
Santiagoes1 <133037528+Santiagoes1@users.noreply.github.com>
2025-03-22 13:17:11 -0500

    Delete cypress directory

--- cypress/support/selectors.js
+++
@@ -1,4 +0,0 @@
-export const usernameSelector = 'input[name="username"]';
-export const passwordSelector = 'input[name="password"]';
-export const loginButtonSelector = 'button[type="submit"]';
-export const credentialsContainer = '.orangehrm-demo-credentials'; // Clase donde aparecen las credenciales en la URL proporcionada
